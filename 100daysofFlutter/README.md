678ac47ff5faaeabe32ed350dad435ea2a2c61fc
Sachin K <sachink@backbase.com>
2021-03-19 18:11:11 +0100

    Day 6:Aligning as per design.

--- 100daysofFlutter/README.md
+++ 100daysofFlutter/README.md
@@ -14,3 +14,46 @@ Note: After much time figured out SVG is not yet supported. :open_mouth::open_mo
 Screenhot 
 ![Screenshot](https://github.com/SachinKasaraddi/100daysofFlutter/blob/main/day4/assets/simulator_screenshot_67AFDF90-962A-4B55-9E48-28280DD80958.png)
 
+# Day5
+
+Continued - Layouts in Flutter
+
+Since learning layouts also need exercise to begin with, hence started checking for inspiration and found this.
+ [](https://www.uplabs.com/posts/pet-adoption-app-play-the-prototype-on-uigiants-com#)
+
+Yeah trying to achieve #AndroidDevChallenge via Flutter
+
+ #Dev Challenge 1 : Puppy Adopt app.
+# 100daysCodeChallenge
+# 100daysFlutter
+
+Trying to achieve this design
+
+Learnings
+
+* Dart programming from [](https://www.youtube.com/watch?v=Ej_Pcr4uC2Q)
+
+* Card widget
+* Found Chip Widget but wasn't useful, since its big and I was looking for badge hence had to write custom one.
+* First logic introduced to check whether puppy is Male or Female.
+* Curved corner radius used for cards.
+
+Screenhot
+![Screenshot](https://github.com/SachinKasaraddi/100daysofFlutter/blob/main/day4/assets/Day-5-screenshot.png)
+
+# Day6
+
+Continued - Layouts in Flutter
+# 100daysCodeChallenge
+# 100daysFlutter
+
+Completed the list part of inspiration design
+
+Learnings
+
+* Corner radius of Card was overlapped by image [Fixed using link:](https://stackoverflow.com/questions/53267675/flutter-card-top-radius-is-covered-by-image)
+* Adding custom fonts
+* More time invested on aligning as per design in app.
+
+
+
--- 100daysofFlutter/lib/Dashboard.dart
+++ 100daysofFlutter/lib/Dashboard.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Dashboard extends StatelessWidget {
@@ -9,48 +10,67 @@ class Dashboard extends StatelessWidget {
       crossAxisSpacing: 10,
       mainAxisSpacing: 10,
       crossAxisCount: 2,
+      childAspectRatio: 0.75,
       children: <Widget>[
+        _puppyItem("assets/images/pet3.jpeg", "Puppy", true, "Pippa", "Border colli"),
+        _puppyItem("assets/images/pet4.jpeg", "Puppy", true, "Pickle", "Bulldog"),
+        _puppyItem("assets/images/pet5.jpeg", "Adult", false, "Blossom", "Maltese puddle"),
+        _puppyItem("assets/images/pet6.jpeg", "Puppy", true, "Hailey", "Pitbull"),
+        _puppyItem("assets/images/pet7.jpeg", "Puppy", true, "Kiki", "Spaniel"),
+        _puppyItem("assets/images/pet8.jpeg", "Adult", false, "Missy", "Cross"),
         _puppyItem("assets/images/pet1.jpeg", "Puppy", true, "Nora", "Corgi"),
         _puppyItem("assets/images/pet2.jpeg", "Adult", false, "Basil", "Cross"),
-        _puppyItem("assets/images/pet3.jpeg", "Puppy", true, "Nora", "Corgi"),
-        _puppyItem("assets/images/pet4.jpeg", "Puppy", true, "Basil", "Cross"),
-        _puppyItem("assets/images/pet5.jpeg", "Adult", false, "Nora", "Corgi"),
-        _puppyItem("assets/images/pet6.jpeg", "Puppy", true, "Basil", "Cross"),
-        _puppyItem("assets/images/pet7.jpeg", "Puppy", true, "Nora", "Corgi"),
-        _puppyItem("assets/images/pet8.jpeg", "Adult", false, "Basil", "Cross"),
-        _puppyItem("assets/images/pet9.jpeg", "Puppy", true, "Basil", "Cross"),
+        _puppyItem("assets/images/pet9.jpeg", "Puppy", true, "Tessa", "Border colli"),
       ],
     );
 
     return MaterialApp(
       title: 'Flutter Demo',
       home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.white,
-            elevation: 0,
-            title: Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                'Adopt me!',
-                style: TextStyle(color: Colors.black),
-              ),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          title: Align(
+            alignment: Alignment.topLeft,
+            child: Text(
+              'Adopt Me!',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontFamily: 'Roboto-Bold',
+                  fontWeight: FontWeight.w500),
             ),
           ),
-          body: puppiesList),
+        ),
+        body: Container(
+          color: Colors.white,
+          child: puppiesList,
+        ),
+      ),
     );
   }
 
   Card _puppyItem(String imagePath, String puppyType, bool isMale, String name,
       String breed) {
     return Card(
-      elevation: 2,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+      elevation: 1,
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
         children: [
-          Expanded(
-            child: Image.asset(imagePath),
+          Container(
+            child: Image.asset(
+              imagePath,
+              height: 150,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            decoration: BoxDecoration(),
           ),
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Card(
                 color: isMale ? const Color(0xFFFAE6E6) : Color(0xFFE2EEF9),
@@ -59,15 +79,38 @@ class Dashboard extends StatelessWidget {
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(15.0)),
                 child: Padding(
-                    padding: const EdgeInsets.only(
-                        left: 15, top: 4, right: 15, bottom: 4),
-                    child: Text(
-                      puppyType,
-                      style: TextStyle(
-                          color: isMale ? Color(0xFF104A7D) : Color(0xFF104A7D)),
-                    )),
-              )
+                  padding: const EdgeInsets.only(
+                      left: 15, top: 4, right: 15, bottom: 4),
+                  child: Text(
+                    puppyType,
+                    style: TextStyle(
+                      color: isMale ? Color(0xFF8A6A18) : Color(0xFF104A7D),
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                ),
+              ),
             ],
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 8),
+            child: Text(
+              name,
+              style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 20,
+                  fontFamily: 'Roboto-Bold'),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(8),
+            child: Text(
+              breed,
+              style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w300,
+                  fontFamily: 'Roboto-Light'),
+            ),
           ),
         ],
       ),
