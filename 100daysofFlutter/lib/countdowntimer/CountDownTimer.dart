21f860fa080c86e6018209a2014f438d61ac4c8a
Sachin K <sachink@backbase.com>
2021-03-31 21:25:10 +0200

    Day 11:Heading towards custom panint Canvas in flutter

--- 100daysofFlutter/lib/countdowntimer/CountDownTimer.dart
+++ 100daysofFlutter/lib/countdowntimer/CountDownTimer.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-import 'package:path_drawing/path_drawing.dart';
+import 'dart:math';
 
 class CountDownTimer extends StatefulWidget {
   @override
@@ -9,63 +8,13 @@ class CountDownTimer extends StatefulWidget {
 
 class _CountDownTimerState extends State<CountDownTimer>
     with SingleTickerProviderStateMixin {
-  int remainingTime = 10;
-  AnimationController _animationController;
-  Animation<double> _animation;
-
-  Timer _timer;
-  final textController = TextEditingController();
-
   @override
   void initState() {
     super.initState();
-
-    _animationController = new AnimationController(
-      vsync: this,
-      duration: Duration(seconds: remainingTime),
-    );
-    _animation = Tween(begin: 0.0, end: 1.0).animate(
-        CurvedAnimation(parent: _animationController, curve: Curves.linear));
-  }
-
-  void startCountDownTimer(int remainingTimeValue) {
-    if (isTimerStarted()) {
-      remainingTime = remainingTimeValue;
-      const oneSec = const Duration(seconds: 1);
-      _timer = new Timer.periodic(
-        oneSec,
-        (Timer timer) {
-          if (remainingTime == 0) {
-            setState(() {
-              timer.cancel();
-              remainingTime = 0;
-            });
-          } else {
-            setState(() {
-              remainingTime--;
-            });
-          }
-        },
-      );
-    } else {
-      setState(() {
-        _timer.cancel();
-        remainingTime = 0;
-      });
-    }
-
-    if (_animationController.isAnimating) {
-      _animationController.stop();
-    } else {
-      _animationController.reset();
-      _animationController.forward();
-    }
   }
 
   @override
   void dispose() {
-    _timer.cancel();
-    textController.dispose();
     super.dispose();
   }
 
@@ -76,93 +25,71 @@ class _CountDownTimerState extends State<CountDownTimer>
         backgroundColor: Colors.red,
         body: Align(
           alignment: Alignment.centerRight,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                width: 150,
-                height: 50,
-                child: TextFormField(
-                  controller: textController,
-                  decoration: InputDecoration(
-                    labelText: 'Enter time',
-                    labelStyle: TextStyle(color: Colors.white),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                    ),
-                  ),
-                  style: TextStyle(color: Colors.white),
-                ),
+          child: Container(
+            margin: EdgeInsets.only(top: 400),
+            width: 400,
+            height: 400,
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: CustomPaint(
+                painter: MyPainter(context, 700, 20, DateTime.now()),
               ),
-              ElevatedButton(
-                onPressed: () {
-                  startCountDownTimer(int.parse(textController.text));
-                },
-                child: isTimerStarted() ? Text("Start") : Text("Stop"),
-              ),
-              Text(
-                isTimerStarted() ? "Press start" : "$remainingTime",
-                style: TextStyle(color: Colors.white),
-              ),
-              Align(
-                alignment: Alignment.topRight,
-                child: AnimatedBuilder(
-                    animation: _animation,
-                    child: CustomPaint(
-                      size: Size(300, 300),
-                      painter: MyPainter(),
-                    ),
-                    builder: (context, child) {
-                      return Transform.rotate(
-                        angle: _animation.value,
-                        child: child,
-                      );
-                    }),
-              ),
-            ],
+            ),
           ),
         ),
       ),
     );
   }
-
-  bool isTimerStarted() => remainingTime == 10 || remainingTime == 0;
 }
 
 class MyPainter extends CustomPainter {
   //         <-- CustomPainter class
+  BuildContext context;
+  final indicator = Paint()
+    ..color = Color(0xFF007CFA)
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = 2;
+
+  final shortNeedle = Paint()
+    ..style = PaintingStyle.fill
+    ..strokeWidth = 4
+    ..color = Colors.white;
+
+  Color gradientPatchColor;
+  double wheelSize;
+  double radius;
+  double shortNeedleHeight;
+  final DateTime dateTime;
+
+  MyPainter(
+    this.context,
+    this.wheelSize,
+    this.shortNeedleHeight,
+    this.dateTime,
+  ) {
+    gradientPatchColor = Theme.of(context).scaffoldBackgroundColor;
+    radius = wheelSize / 2;
+  }
+
+  double degreeToRadians(double degrees) => degrees * (pi / 180);
+
+  double radianToDegrees(double radians) => radians * (180 / pi);
+
   @override
   void paint(Canvas canvas, Size size) {
-    var paint = Paint();
-
-    paint.color = Colors.black;
-    paint.style = PaintingStyle.stroke;
-    paint.strokeWidth = 3;
-
-    var startPoint = Offset(0, size.height / 2);
-    var controlPoint1 = Offset(size.width / 4, size.height / 3);
-    var controlPoint2 = Offset(3 * size.width / 4, size.height / 3);
-    var endPoint = Offset(size.width, size.height / 2);
-
-    var path = Path();
-    path.moveTo(startPoint.dx, startPoint.dy);
-    path.cubicTo(controlPoint1.dx, controlPoint1.dy, controlPoint2.dx,
-        controlPoint2.dy, endPoint.dx, endPoint.dy);
-
-    canvas.drawPath(
-      dashPath(
-        path,
-        dashArray: CircularIntervalList<double>(<double>[15.0, 10.5]),
-      ),
-      paint,
-    );
+    wheelSize = size.width * 0.5;
+    for (double i = 0; i <= 360; i++) {
+      if (i % 20 == 0) {
+        canvas.drawLine(
+            Offset(radius * cos(degreeToRadians(i)),
+                radius * sin(degreeToRadians(i))),
+            Offset((radius - shortNeedleHeight) * cos(degreeToRadians(i)),
+                (radius - shortNeedleHeight) * sin(degreeToRadians(i))),
+            shortNeedle);
+      }
+    }
   }
 
   @override
-  bool shouldRepaint(CustomPainter old) {
-    return false;
-  }
+  bool shouldRepaint(CustomPainter old) => false;
 }
